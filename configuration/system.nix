50a8472bcf0c6b18047d3ca7723a944ac99a1e4d
installer <installer@gmail.com>
2025-10-19 11:59:42 +0200

    Rollbacked system

--- configuration/system.nix
+++ configuration/system.nix
@@ -1,67 +1,92 @@
-{
-  inputs,
-  config,
-  pkgs,
-  host,
-  username,
-  options,
-  lib,
-  system,
-  ...
-}:
+# 💫 https://github.com/JaKooLit 💫 #
+# Main default config
+
+
+# NOTE!!! : Packages and Fonts are configured in packages-&-fonts.nix
+
+
+{ config, pkgs, host, username, options, lib, system, ...}:
+let
+  
+  inherit (import ./variables.nix) keyboardLayout;
+  #home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz; 
+in
 {
   imports = [
     ./hardware.nix
-    ./specialisation.nix
     ./user.nix
-    ./terminal.nix
     ./packages.nix
+    ./home.nix
+    ./specialisation.nix
     ./quickshell.nix
-    ../modules/intel-gpu.nix
+    ../modules/amd-drivers.nix
+    #../../modules/nvidia-drivers.nix
+    #../../modules/nvidia-prime-drivers.nix
+    ../modules/intel-drivers.nix
+    ../modules/vm-guest-services.nix
+    ../modules/local-hardware-clock.nix
+    #(import "${home-manager}/nixos")
   ];
 
   # BOOT related stuff
   boot = {
     kernelPackages = pkgs.linuxPackages_zen; # zen Kernel
+    #kernelPackages = pkgs.linuxPackages_latest; # Kernel 
 
     kernelParams = [
-      "nowatchdog"
-      "modprobe.blacklist=iTCO_wdt" # watchdog for Intel
+      "systemd.mask=systemd-vconsole-setup.service"
+      "systemd.mask=dev-tpmrm0.device" #this is to mask that stupid 1.5 mins systemd bug
+      "nowatchdog" 
+      "modprobe.blacklist=sp5100_tco" #watchdog for AMD
+      "modprobe.blacklist=iTCO_wdt" #watchdog for Intel
       "nvidia-drm.modeset=1"
     ];
 
-    initrd = {
-      availableKernelModules = [
-        "xhci_pci"
-        "ahci"
-        "nvme"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-      ];
+    # This is for OBS Virtual Cam Support
+    #  extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    
+    initrd = { 
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
       kernelModules = [ ];
     };
 
-    # Bootloader GRUB
-    loader.efi.canTouchEfiVariables = true;
-    loader.grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-      gfxmodeBios = "auto";
-      memtest86.enable = true;
-      #extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
-      configurationName = "${host}";
-      theme = inputs.nixos-grub-themes.packages.${pkgs.system}.hyperfluent;
-      #efiInstallAsRemovable = true;
-    };
+    # Needed For Some Steam Games
+    #kernel.sysctl = {
+    #  "vm.max_map_count" = 2147483642;
+    #};
 
+    ## BOOT LOADERS: NOTE USE ONLY 1. either systemd or grub  
+    # Bootloader SystemD
+    #loader.systemd-boot.enable = true;
+  
+    loader.efi = {
+	    #efiSysMountPoint = "/efi"; #this is if you have separate /efi partition
+	    canTouchEfiVariables = true;
+  	  };
+
+    loader.timeout = 5;    
+  			
+    # Bootloader GRUB
+    loader.grub = {
+	    enable = true;
+	      devices = [ "nodev" ];
+	      efiSupport = true;
+        gfxmodeBios = "auto";
+	      memtest86.enable = true;
+	    #  extraGrubInstallArgs = [ "--bootloader-id=${host}" ];
+	      configurationName = "${host}";
+  	  	 };
+
+    # Bootloader GRUB theme, configure below
+
+    ## -end of BOOTLOADERS----- ##
+  
     # Make /tmp a tmpfs
     tmp = {
       useTmpfs = false;
       tmpfsSize = "30%";
-    };
-
+      };
+    
     # Appimage Support
     binfmt.registrations.appimage = {
       wrapInterpreterInShell = false;
@@ -70,55 +95,71 @@
       offset = 0;
       mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
       magicOrExtension = ''\x7fELF....AI\x02'';
-    };
-
+      };
+    
     plymouth = {
-      enable = false;
-      theme = "red_loader";
-      themePackages = with pkgs; [
+	enable = true;
+	theme = "red_loader";
+        themePackages = with pkgs; [
         # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "red_loader" ];
-        })
-      ];
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ "red_loader" ];
+          })
+        ];
     };
   };
+
+  # GRUB Bootloader theme. Of course you need to enable GRUB above.. duh! and also, enable it on flake.nix
+  #distro-grub-themes = {
+  #  enable = true;
+  #  theme = "nixos";
+  #};
+
+  # Extra Module Options
+  drivers.amdgpu.enable = false;
+  drivers.intel.enable = true;
+  #drivers.nvidia.enable = true;
+  #drivers.nvidia-prime = {
+   # enable = true;
+    #intelBusID = "PCI:0:2:0";
+    #nvidiaBusID = "PCI:45:0:0";
+  #};
+  vm.guest-services.enable = false;
+  local.hardware-clock.enable = false;
 
   # networking
   networking.networkmanager.enable = true;
   networking.hostName = "${host}";
   networking.timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
   networking.firewall.checkReversePath = "loose";
-  networking.nameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
-
-  environment.etc."resolv.conf".text = lib.mkForce ''
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+environment.etc."resolv.conf".text = lib.mkForce ''
     nameserver 192.168.1.10
     nameserver 8.8.8.8
     nameserver 1.1.1.1
-  '';
+'';
 
   services.resolved = {
     enable = true;
     dnssec = "false";
     domains = [ "~." ];
-    fallbackDns = [
-      "1.1.1.1"
-      "8.8.8.8"
-    ];
+    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
     dnsovertls = "false";
   };
 
-  time.timeZone = "Europe/Paris";
-
+  # Set your time zone.
+  # services.automatic-timezoned.enable = true; #based on IP location
+  
+  #https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  time.timeZone = "Europe/Paris"; # Set local timezone
+ 
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "fr_FR.UTF-8/UTF-8"
-  ];
-
+      "en_US.UTF-8/UTF-8"
+      "fr_FR.UTF-8/UTF-8"
+    ];
+  
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
@@ -131,31 +172,36 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration # Comment out this line if you use KDE Connect
-    kdepim-runtime # Unneeded if you use Thunderbird, etc.
-    konsole # Comment out this line if you use KDE's default terminal app
-    oxygen
-    dolphin
-    ark
-    elisa
-    gwenview
-    okular
-    kate
-    dolphin-plugins
-    krdp
-  ];
+  environment.etc."greetd/environments".text = ''
+     Hyprland
+     startplasma-wayland
+  '';
+
+   environment.plasma6.excludePackages = with pkgs.kdePackages; [
+      plasma-browser-integration # Comment out this line if you use KDE Connect
+      kdepim-runtime # Unneeded if you use Thunderbird, etc.
+      konsole # Comment out this line if you use KDE's default terminal app
+      oxygen
+      dolphin
+      ark
+      elisa
+      gwenview
+      okular
+      kate
+      dolphin-plugins
+      krdp
+    ];
 
   # Services to start
   services = {
     xserver = {
       enable = false;
-      xkb = {
+     xkb = {
         layout = "us";
         variant = "";
       };
     };
-    
+    #displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
 
     displayManager.sddm.enable = true;
@@ -176,55 +222,124 @@
 	#default_session = initial_session;
       };
     };
-
+    
     smartd = {
       enable = false;
       autodetect = true;
     };
+    
+	  gvfs.enable = true;
+	  tumbler.enable = true;
 
-    gvfs.enable = true;
-    tumbler.enable = true;
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      wireplumber.enable = true;
-    };
-
-    pulseaudio.enable = false;
-
+	  pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+	    wireplumber.enable = true;
+  	  };
+	
+    	pulseaudio.enable = false; #unstable
     udev = {
-      enable = true;
-      packages = [ pkgs.libsigrok ];
+       enable = true;
+    packages = [ pkgs.libsigrok ];     
     };
+	  envfs.enable = true;
+	  dbus.enable = true;
 
-    envfs.enable = true;
-    dbus.enable = true;
-
+	  fstrim = {
+      enable = true;
+      interval = "weekly";
+      };
+  
     libinput.enable = true;
 
     rpcbind.enable = false;
     nfs.server.enable = false;
-
+  
     openssh.enable = true;
     flatpak.enable = true;
+	
+  	blueman.enable = true;
+  	
+  	#hardware.openrgb.enable = true;
+  	#hardware.openrgb.motherboard = "amd";
 
-    fwupd.enable = true;
+	  fwupd.enable = true;
+
+	  upower.enable = true;
+    
     gnome.gnome-keyring.enable = true;
+    
+    #printing = {
+    #  enable = false;
+    #  drivers = [
+        # pkgs.hplipWithPlugin
+    #  ];
+    #};
+    
+    #avahi = {
+    #  enable = true;
+    #  nssmdns4 = true;
+    #  openFirewall = true;
+    #};
+    
+    #ipp-usb.enable = true;
+    
+    #syncthing = {
+    #  enable = false;
+    #  user = "${username}";
+    #  dataDir = "/home/${username}";
+    #  configDir = "/home/${username}/.config/syncthing";
+    #};
+
   };
-
-  environment.etc."greetd/environments".text = ''
-    Hyprland
-    startplasma-wayland
-  '';
-
+  
   systemd.services.flatpak-repo = {
     path = [ pkgs.flatpak ];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
+  };
+
+  # zram
+  zramSwap = {
+	  enable = true;
+	  priority = 100;
+	  memoryPercent = 30;
+	  swapDevices = 1;
+    algorithm = "zstd";
+    };
+  services.power-profiles-daemon.enable = true;
+  powerManagement = {
+  	enable = true;
+	  cpuFreqGovernor = "schedutil";
+  };
+
+  #hardware.sane = {
+  #  enable = true;
+  #  extraBackends = [ pkgs.sane-airscan ];
+  #  disabledDefaultBackends = [ "escl" ];
+  #};
+
+  # Extra Logitech Support
+  hardware.logitech.wireless.enable = false;
+  hardware.logitech.wireless.enableGraphical = false;
+
+  #services.pulseaudio.enable = true; # stable branch
+
+  # Bluetooth
+  hardware = {
+  	bluetooth = {
+	    enable = true;
+	    powerOnBoot = true;
+	    settings = {
+		    General = {
+		      Enable = "Source,Sink,Media,Socket";
+		      Experimental = true;
+		    };
+      };
+    };
   };
 
   # Security / Polkit
@@ -246,13 +361,11 @@
       }
     })
   '';
-
-  # Unsure if still needed
-  # security.pam.services.swaylock = {
-  #   text = ''
-  #     auth include login
-  #   '';
-  # };
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
 
   # Cachix, Optimization settings and garbage collection automation
   nix = {
@@ -262,11 +375,9 @@
         "nix-command"
         "flakes"
       ];
-
       substituters = [ "https://hyprland.cachix.org" ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
-
     gc = {
       automatic = true;
       dates = "weekly";
@@ -274,11 +385,29 @@
     };
   };
 
-  console.keyMap = "us";
+  # Virtualization / Containers
+  #virtualisation.libvirtd.enable = false;
+  virtualisation.podman = {
+    enable = false;
+    dockerCompat = false;
+    defaultNetwork.settings.dns_enabled = false;
+  };
+
+  # OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  console.keyMap = "${keyboardLayout}";
 
   # For Electron apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -287,4 +416,57 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+
+  #users.users.whitebow.isNormalUser = true;
+  #home-manager.users.whitebow = { pkgs, ... }: {
+  #  home.packages = [ pkgs.atool pkgs.httpie ];
+  #  programs.bash.enable = true;
+  
+    # The state version is required and should stay at the version you
+    # originally installed.
+  #  home.stateVersion = "25.05";
+  #};
+ # systemd.services."getty@tty2" = {
+ # enable = true;
+  # Replace "yourusername" with your actual username
+ # serviceConfig.ExecStart = [
+ #   "" # Override the default ExecStart
+ #   "${pkgs.utillinux}/sbin/agetty --autologin whitebow --noclear %I $TERM"
+ # ];
+  # Ensure this starts after the graphical stack is ready
+ # after = [ "graphical.target" ];
+ # wantedBy = [ "multi-user.target" ];
+#};
+
+# Auto-start Hyprland when logging in on tty2
+#environment.interactiveShellInit = ''
+ # if [[ "$(tty)" == "/dev/tty2" ]]; then
+  #  exec dbus-run-session /home/whitebow/.nix-profile/bin/Hyprland
+  #fi
+#
+#  systemd.services."display-manager" = {
+    #serviceConfig = {
+     # ExecStart = [
+        #"${pkgs.kwrite}/bin/kwrite" # This is just a placeholder
+        #""
+      #  "${pkgs.kdePackages.sddm}/bin/sddm"
+     # ];
+    #  Restart = "always";
+      #RestartSec = "1s";
+   #   StandardInput = "tty";
+      #StandardOutput = "tty";
+     # TTYPath = "/dev/tty2";
+    #  TTYReset = "yes";
+     # TTYVHangup = "yes";
+    #  TTYVTDisallocate = "yes";
+   # };
+    #wantedBy = [ "multi-user.target" ];
+   # after = [ "systemd-user-sessions.service" "plymouth-quit-wait.service" ];
+  #};
+
+#services.getty.autologinUser = null;
+ # services.getty.helpLine = "";
+  #services.getty.greetingLine = "";
+  #systemd.services."getty@tty2".enable = false;
+  #systemd.services."getty@tty3".enable = false;
 }
